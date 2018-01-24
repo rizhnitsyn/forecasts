d103811baa71fe86b325c46954723a126c1655f9
rizhnitsyn <Wegawega2>
2018-01-24 15:57:54 +0300

    config hibernate+mysql

--- database/src/test/java/by/forecasts/UserDaoTest.java
+++ database/src/test/java/by/forecasts/UserDaoTest.java
@@ -1,12 +1,27 @@
 package by.forecasts;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UserDaoTest {
+    private static final SessionFactory SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
 
     @Test
     public void getUserNameTest() {
         Assert.assertEquals("Demo User", new UserDao().getUserName());
+    }
+
+    @Test
+    public void userHibernateTest() {
+        Session session = SESSION_FACTORY.openSession();
+
+        Team team = session.find(Team.class, 22L);
+        System.out.println(team);
+
+        session.close();
+        SESSION_FACTORY.close();
     }
 }
