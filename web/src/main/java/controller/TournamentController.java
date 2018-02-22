6e8fecedb43b06f75cbdc696abf39a839102ca57
rizhnitsyn <enotius@tut.by>
2018-02-22 23:31:18 +0300

    creating controller Tournament

--- web/src/main/java/controller/TournamentController.java
+++ web/src/main/java/controller/TournamentController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import by.forecasts.config.ApplicationContextHolder;
+import by.forecasts.service.TournamentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TournamentController {
+
+    @GetMapping("/tournament")
+    public String getTournamentById(Model model) {
+        TournamentService tournamentService = ApplicationContextHolder.getBean(TournamentService.class);
+        model.addAttribute("tournament", tournamentService.findOne(1L));
+        return "show_tournament";
+    }
 }
