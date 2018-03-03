b46a80d1e2263f0015e891996a7c639d5f9587fa
rizhnitsyn <enotius@tut.by>
2018-03-03 17:14:57 +0300

    creating GroupType entity

---
+++ database/src/main/java/by/forecasts/repositories/GroupTypeRepository.java
@@ -0,0 +1,7 @@
+package by.forecasts.repositories;
+
+import by.forecasts.entities.GroupType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupTypeRepository extends JpaRepository<GroupType, Long> {
+}
