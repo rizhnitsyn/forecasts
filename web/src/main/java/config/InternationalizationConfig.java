c800a6a23d917ab02e38af75d6f360608da0e86a
rizhnitsyn <enotius@tut.by>
2018-03-04 16:11:41 +0300

    creating header, footer, menu

--- web/src/main/java/config/InternationalizationConfig.java
+++ web/src/main/java/config/InternationalizationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -40,5 +41,16 @@ public class InternationalizationConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/css/**",
+                "/jpg/**",
+                "/js/**")
+                .addResourceLocations("classpath:/css/",
+                        "classpath:/jpg/",
+                        "classpath:/js/");
     }
 }
