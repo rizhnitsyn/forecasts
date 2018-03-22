6ef2cddde25977702c0a59c42994a83085f51fa1
rizhnitsyn <enotius@tut.by>
2018-03-22 17:40:31 +0300

    tests uncommitted

--- service/src/test/java/by/forecasts/service/service/MatchServiceTest.java
+++ service/src/test/java/by/forecasts/service/service/MatchServiceTest.java
@@ -20,34 +20,34 @@ public class MatchServiceTest extends BaseServiceTest {
         System.out.println(allByTournamentIdAndGroupId);
     }
 
-//    @Test
-//    public void findAllByTournamentIdAndGroupIdTest() {
-//        List<Match> matches = matchService.findAllByTournamentIdAndGroupId(1L,48L);
-//        assertThat(matches.size(), Matchers.equalTo(1));
-//        assertEquals(matches.iterator().next().getSecondTeam().getTeamName(),"Франция");
-//    }
+    @Test
+    public void findAllByTournamentIdAndGroupIdTest() {
+        List<Match> matches = matchService.findAllByTournamentIdAndGroupId(1L,48L);
+        assertThat(matches.size(), Matchers.equalTo(1));
+        assertEquals(matches.iterator().next().getSecondTeam().getTeamName(),"Франция");
+    }
 
-//    @Test
-//    public void findByIdTest() {
-//        MatchHardViewDto match = matchService.findById(100L, 1L);
-//        assertEquals(match.getSecondTeam(),"Франция");
-//    }
+    @Test
+    public void findByIdTest() {
+        MatchHardViewDto match = matchService.findById(100L, 1L);
+        assertEquals(match.getSecondTeam(),"Франция");
+    }
 
-//    @Test
-//    public void addMatchScoreTest() {
-//        MatchHardViewDto byId = matchService.findById(100L, 1L);
-//        MatchScore matchScore = new MatchScore(2, 3);
-//        matchService.addMatchScore(100L, matchScore);
-//        Match one = matchService.findOne(100L);
-//        assertThat(byId.getMatchScore().getSecondResult(), Matchers.equalTo(0));
-//        assertThat(one.getMatchFinalResult().getSecondResult(), Matchers.equalTo(3));
-//    }
+    @Test
+    public void addMatchScoreTest() {
+        MatchHardViewDto byId = matchService.findById(100L, 1L);
+        MatchScore matchScore = new MatchScore(2, 3);
+        matchService.addMatchScore(100L, matchScore);
+        Match one = matchService.findOne(100L);
+        assertThat(byId.getMatchScore().getSecondResult(), Matchers.equalTo(0));
+        assertThat(one.getMatchFinalResult().getSecondResult(), Matchers.equalTo(3));
+    }
 
-//    @Test
-//    public void findAllByTournamentIdUserIdTest() {
-//        List<MatchShortViewDto> matches = matchService.findAllByTournamentIdUserId(1L, 1L);
-//        assertThat(matches.size(), Matchers.equalTo(51));
-//    }
+    @Test
+    public void findAllByTournamentIdUserIdTest() {
+        List<MatchShortViewDto> matches = matchService.findAllByTournamentIdUserId(1L, 1L);
+        assertThat(matches.size(), Matchers.equalTo(51));
+    }
 
     @Test
     public void findAllByTournamentIdUserIdPageableTest() {
@@ -63,9 +63,9 @@ public class MatchServiceTest extends BaseServiceTest {
         assertEquals(i, 6);
     }
 
-//    @Test
-//    public void findMatchesOfSelectedTeamTest() {
-//        List<MatchShortViewDto> matches = matchService.findMatchesOfSelectedTeam(1L, 1L);
-//        assertThat(matches, Matchers.hasSize(7));
-//    }
+    @Test
+    public void findMatchesOfSelectedTeamTest() {
+        List<MatchShortViewDto> matches = matchService.findMatchesOfSelectedTeam(1L, 1L);
+        assertThat(matches, Matchers.hasSize(7));
+    }
 }
